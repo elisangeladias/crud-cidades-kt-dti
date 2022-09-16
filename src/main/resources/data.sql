faf86d19ed2fad113575107e2e4a690ce545e1de
Elis PosUTFPR <elisangelacwb@hotmail.com>
2022-09-16 14:52:36 -0300

    projeto completo apresentado a minha equipe na empresa dti

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+
+INSERT INTO `usuario` VALUES (1,'user','$2a$10$/.KKJU.G71/Fl.4wKo.lJOfDhxhHPo0o.DPxIy98IuKSaK74WXUy2');
+INSERT INTO `usuario` VALUES (2,'adm','$2a$10$/.KKJU.G71/Fl.4wKo.lJOfDhxhHPo0o.DPxIy98IuKSaK74WXUy2');
+
+INSERT INTO `usuario_papeis` VALUES (1, 'listar');
+INSERT INTO `usuario_papeis` VALUES (2, 'listar');
+INSERT INTO `usuario_papeis` VALUES (2, 'admin');
